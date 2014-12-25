da1d8c756c5a55ea8141d66f8bb7f46637cb7d35
Stefan Brus <stefan1brus@gmail.com>
2014-12-26 00:32:48 +0100

    Rethink :calc command parser grammar
    Fixes #31

--- src/Calc.hs
+++ src/Calc.hs
@@ -29,31 +29,53 @@ evalExpr (Div e1 e2) = evalExpr e1 / evalExpr e2
 calculate :: String -> Either ParseError Double
 calculate s = evalExpr <$> parse expr "" s
 
+-- Build an expression tree from the given list of operator/expression pairs
+foldExpr :: Expr -> [(Char,Expr)] -> Expr
+foldExpr e = foldl build e
+  where
+    build :: Expr -> (Char,Expr) -> Expr
+    build res (op,ex) = case op of
+      '+' -> Add res ex
+      '-' -> Sub res ex
+      '*' -> Mul res ex
+      '/' -> Div res ex
+      _ -> error "Unexpected operator"
+
 -- Parse an expression
 expr :: Parser Expr
 expr = do
   whitespace
-  try term <|> try factor <|> number
+  term
 
 -- Parse a term
 term :: Parser Expr
 term = do
-  t1 <- try factor <|> number
-  whitespace
-  op <- oneOf "+-"
-  whitespace
-  t2 <- try term <|> try factor <|> number
-  if op == '+' then return $ Add t1 t2 else return $ Sub t1 t2
+  t1 <- factor
+  ops <- many termOp
+  return $ foldExpr t1 ops
+  where
+    termOp :: Parser (Char,Expr)
+    termOp = do
+      whitespace
+      op <- oneOf "+-"
+      whitespace
+      t2 <- factor
+      return (op,t2)
 
 -- Parse a factor
 factor :: Parser Expr
 factor = do
   n1 <- number
-  whitespace
-  op <- oneOf "*/"
-  whitespace
-  n2 <- try factor <|> number
-  if op == '*' then return $ Mul n1 n2 else return $ Div n1 n2
+  ops <- many facOp
+  return $ foldExpr n1 ops
+  where
+    facOp :: Parser (Char,Expr)
+    facOp = do
+      whitespace
+      op <- oneOf "*/"
+      whitespace
+      n2 <- number
+      return (op,n2)
 
 -- Parse a number
 number :: Parser Expr
